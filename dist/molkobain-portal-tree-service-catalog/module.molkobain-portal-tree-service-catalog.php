649f9f9c9dead66d3972b8924a00e5e7716fad32
Molkobain <lajarige.guillaume@free.fr>
2018-04-18 15:36:16 +0200

    Add extension.xml file

--- dist/molkobain-portal-tree-service-catalog/module.molkobain-portal-tree-service-catalog.php
+++ dist/molkobain-portal-tree-service-catalog/module.molkobain-portal-tree-service-catalog.php
@@ -8,7 +8,7 @@ SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
 	'molkobain-portal-tree-service-catalog/1.0.0', array(
 	// Identification
-	'label' => 'Portal: Service catalog as tree by default',
+	'label' => 'Portal: Service catalog as tree view',
 		'category' => 'Portal',
 	// Setup
 	'dependencies' => array(
